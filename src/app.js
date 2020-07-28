dd39333e872259646d6c12ce356fe1cde7f2b548
Filipe Boechat <filipeboechatsampaio@hotmail.com>
2020-07-28 08:49:06 -0300

    challenge solved

--- src/app.js
+++ src/app.js
@@ -1,33 +1,91 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+ return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repo)
+
+  return response.json(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repo = repositories.find((repository) => repository.id === id);
+
+  if (!repo) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  const newRepo = {
+    ...repo,
+    title: title || repo.title,
+    url: url || repo.url,
+    techs: techs || repo.techs,
+  };
+
+  repositories = repositories.filter((repository) =>
+    repository.id === id ? newRepo : repository
+  );
+
+  return response.json(newRepo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repo = repositories.find((repository) => repository.id === id);
+
+  if (!repo) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  repositories = repositories.filter((repository) => !(repository.id === id));
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repo = repositories.find((repository) => repository.id === id);
+
+  if (!repo) {
+    return response.status(400).json({ error: "Repository not found." });
+  }
+
+  const newRepo = {
+    ...repo,
+    likes: repo.likes += 1
+  };
+
+  repositories = repositories.filter((repository) =>
+    repository.id === repo.id ? newRepo : repository
+  );
+
+  return response.json(newRepo);
 });
 
 module.exports = app;
